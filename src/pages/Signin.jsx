d32f6594bf511afbfc6efcca883c38660b30ca1b
kelebekkadircan <kelebekkadircan@gmail.com>
2023-10-21 20:43:46 +0300

    youtube_clone_ui_day2

---
+++ src/pages/Signin.jsx
@@ -0,0 +1,102 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const Container = styled.div`
+display : flex ; 
+align-items: center ; 
+justify-content : center ;
+height : calc(100vh - 56px);
+color: ${({ theme }) => theme.text};
+
+`
+
+const Wrapper = styled.div`
+display : flex ; 
+align-items: center ; 
+flex-direction : column ;
+background-color: ${({ theme }) => theme.bgLighter};
+border: 1px solid ${({ theme }) => theme.soft};
+padding: 20px 50px ;
+gap : 10px;
+
+`
+
+const Title = styled.h1`
+font-size : 24px;
+
+`
+const SubTitle = styled.h2`
+
+font-size : 20px ;
+font-weight : 300 ;
+`
+const Input = styled.input`
+    border: 1px solid ${({ theme }) => theme.soft} ;
+    border-radius : 3px ;
+    padding : 10px;
+    background-color : transparent;
+    width : 100% ;
+`
+
+const Button = styled.button`
+    border-radius  : 3px ; 
+    border: none ; 
+    padding : 10px 20px;
+    font-weight:500;
+    cursor : pointer;
+    background-color: ${({ theme }) => theme.soft};
+    color: ${({ theme }) => theme.textSoft};
+`
+const More = styled.div`
+    display : flex ; 
+    margin-top : 10px;
+    font-size : 10px ;
+    color : ${({ theme }) => theme.textSoft};
+`
+const Links = styled.div`
+    margin-left: 50px;
+`
+const Linky = styled.span`
+    margin-left : 30px ;
+`
+
+const Signin = () => {
+    return (
+        <Container>
+            <Wrapper>
+                <Title>
+                    Sign In
+                </Title>
+                <SubTitle>
+                    to continue to Kelebektube
+                </SubTitle>
+                <Input placeholder='username' />
+                <Input type='password' placeholder='password' />
+                <Button>Sign In</Button>
+                <Title>or</Title>
+                <Input placeholder='username' />
+                <Input placeholder='email' />
+                <Input type='password' placeholder='password' />
+                <Button>Sign Up</Button>
+                <More>
+                    English(USA)
+                    <Links>
+                        <Linky>
+                            Help
+                        </Linky>
+                        <Linky>
+                            Privacy
+                        </Linky>
+                        <Linky>
+                            Terms
+                        </Linky>
+                    </Links>
+                </More>
+            </Wrapper>
+        </Container>
+
+    )
+}
+
+export default Signin
